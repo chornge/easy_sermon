3f1798ce9fa423a06b1a83b007d31ecc35defd7a
Christian <Mxlightningx@gmail.com>
2025-07-31 16:28:43 -0500

    Update build script

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 mod devices;
+mod stream;
 
+use anyhow::Result;
 use reqwest::Client;
 use serde_json::json;
 use std::{error::Error, process::Command};
 
-fn main() {
-    use devices::list_input_outputs;
-    list_input_outputs();
+// use crate::stream::start as start_streaming;
+use dotenv::dotenv;
+use std::env;
 
-    let stream = Command::new("uvicorn")
+fn main() -> Result<()> {
+    // Load .env if present
+    dotenv().ok();
+    env::var("VOSK_MODEL_PATH")
+        .expect("Set VOSK_MODEL_PATH environment variable to the Vosk model directory path");
+
+    // Start audio stream
+    // let audio_stream = start_streaming();
+    // if audio_stream.is_err() {
+    //     panic!(
+    //         "Failed to start audio stream: {}",
+    //         audio_stream.unwrap_err()
+    //     );
+    // }
+
+    // Start audio API server
+    let audio_server = Command::new("uvicorn")
         .arg("api.main:app")
         .arg("--host")
         .arg("0.0.0.0")
@@ -18,14 +36,14 @@ fn main() {
         .arg("warning")
         .status()
         .expect("Failed to run api/main.py");
-
-    if !stream.success() {
-        panic!("Run failed");
+    if !audio_server.success() {
+        panic!("API server failed to start");
     }
+
+    Ok(())
 }
 
-#[allow(dead_code)]
-async fn stage_display(verse: &str) -> Result<(), Box<dyn Error>> {
+async fn _stage_display(verse: &str) -> Result<(), Box<dyn Error>> {
     let pro7_p_host = "localhost";
     let pro7_p_port = 49279;
     let client = Client::new();
