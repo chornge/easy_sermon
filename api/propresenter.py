f4e96618b7c1fb02cfbcae811f6cd8c8d63648c8
Dominion Chapel Houston <media@dominionchapel.org>
2025-08-01 21:31:39 -0500

    Verify verses are being sent to Stage Display

--- api/propresenter.py
+++ api/propresenter.py
@@ -1,30 +1,36 @@
-import aiohttp
+import sys
 import asyncio
+import json
 
-PRO7_P_PORT = 54346
 PRO7_P_HOST = "localhost"
-PASSWORD = ""
+PRO7_P_PORT = 54346
 
 
-async def stage_display(text):
-    uri = f"http://{PRO7_P_HOST}:{PRO7_P_PORT}"
+async def stage_display(verse):
+    request_obj = {
+        "url": "v1/stage/message",
+        "method": "PUT",
+        "body": verse,
+        "chunked": False
+    }
+    request_str = json.dumps(request_obj) + "\r\n" # CRLF-terminated JSON
 
-    # Send payload
-    async with aiohttp.ClientSession().put(
-        uri,
-        json={
-            "url": "v1/stage/message",
-            "method": "PUT",
-            "body": "Testing new stage message",
-            "chunked": "false",
-        },
-    ) as response:
-        if response.status == 200:
-            print(f"✅ Sent to Stage Display: {text}")
-        else:
-            print(f"❌ Failed to send to Stage Display: {response.status}")
+    try:
+        reader, writer = await asyncio.wait_for(
+            asyncio.open_connection(PRO7_P_HOST, PRO7_P_PORT), timeout=3
+        )
 
+        writer.write(request_str.encode())
+        await writer.drain()
+        print(f"✅ {verse} is on Stage Display")
+
+        # 🚫 Don't wait for a response
+        writer.close()
+        await writer.wait_closed()
+
+    except Exception as e:
+        print("❌ Error during send:", e)
 
 if __name__ == "__main__":
     asyncio.run(stage_display("Genesis 1:2"))
-    # asyncio.run(stage_display("1 John 1:2"))
+    sys.exit(0)
--- src/propresenter.rs
+++ src/propresenter.rs
@@ -1,19 +1,30 @@
-use reqwest::Client;
 use serde_json::json;
 use std::error::Error;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
 
 #[allow(dead_code)]
 pub async fn stage_display(verse: &str) -> Result<(), Box<dyn Error>> {
     let host = "localhost";
-    let port = 49279;
-    let client = Client::new();
-    let response = client
-        .put(format!("http://{host}:{port}/v1/stage/message"))
-        .header(reqwest::header::CONTENT_TYPE, "application/json")
-        .json(&json!(verse))
-        .send()
-        .await?;
+    let port = 54346;
+    let addr = format!("{}:{}", host, port);
 
-    println!("Verse sent, Response: {response:?}");
+    let request = json!({
+        "url": "v1/stage/message",
+        "method": "PUT",
+        "body": verse,
+        "chunked": false
+    });
+
+    let mut stream = TcpStream::connect(addr).await?;
+
+    let mut request_str = request.to_string();
+    request_str.push_str("\r\n"); // CRLF-terminated JSON
+
+    stream.write_all(request_str.as_bytes()).await?;
+    stream.shutdown().await?;
+
+    println!("✅ {verse} is on Stage Display");
+    
     Ok(())
 }
